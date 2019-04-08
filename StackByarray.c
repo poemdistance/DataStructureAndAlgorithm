fe3865d716c4f522b1f13967b65764e048b2cb46
poemdistance <poemdistance@gmail.com>
2019-04-08 23:49:02 +0800

    栈的数组实现

--- StackByarray.c
+++ StackByarray.c
@@ -24,13 +24,14 @@ int topAndPop(stack s);
 #define minStackSize ( 5 )
 
 struct stackRecord {
-    int capacity;
-    int topOfStack;
-    int *array;
+    int capacity;   /*栈容量*/
+    int topOfStack; /*值指向栈顶元素*/
+    int *array;     /*实际容纳变量的数组指针*/
 };
 
 void makeEmpty( stack s )
 {
+    /*值置-1, 表示栈为空*/
     s->topOfStack = emptyTOS;
 }
 
@@ -42,25 +43,31 @@ stack createStack( int MaxSize )
         exit(1);
     }
 
+    /*创建栈指针*/
     s = malloc( sizeof(struct stackRecord) );
     if( s == NULL ) {
         printf("Malloc failed\n");
         exit(1);
     }
 
+    /*分配实际容纳变量的数组空间*/
     s->array = malloc( sizeof(int) * MaxSize );
     if( s->array == NULL ) {
         printf("s->array: Malloc failed\n");
         exit(1);
     }
 
+    /*记录用户申请的栈空间大小*/
     s->capacity = MaxSize;
     makeEmpty( s );
+
+    /*返回栈指针*/
     return s;
 }
 
 void disposeStack( stack s )
 {
+    /*释放内存空间*/
     if( s != NULL ) {
         free( s->array );
         free( s );
@@ -74,6 +81,7 @@ int isEmpty( stack s )
 
 int isFull( stack s )
 {
+    /*栈顶值到达容量表示满栈了*/
     return s->topOfStack == s->capacity - 1;
 }
 
@@ -84,11 +92,14 @@ void push( int x, stack s )
         exit(1);
     }
 
+    /*赋值然后让栈顶指示元素+1*/
     s->array[++s->topOfStack] = x;
 }
 
 int topAndPop( stack s )
 {
+    /*返回栈顶元素,
+     *并让栈顶指示元素-1表示释放了对应的栈元素空间*/
     if( ! isEmpty(s) )
         return s->array[s->topOfStack--];
 
@@ -98,6 +109,7 @@ int topAndPop( stack s )
 
 void pop( stack s )
 {
+    /*直接让栈顶指示变量-1表示弹出,对应空间被释放*/
     if( ! isEmpty(s) )
         s->topOfStack--;
     else {
@@ -113,8 +125,10 @@ int main()
     int i ;
     int ret;
 
+    /*创建栈, 空间大小9个int*/
     s = createStack( 9 );
 
+    /*以下为测试用例*/
     for( i = 0; i < s->capacity; i++)
         push(i, s);
 
@@ -134,6 +148,7 @@ int main()
     printf("After pop(s): topOfStack=%d\n", s->topOfStack);
     printf("isEmpty: %d\n", isEmpty(s));
 
+    /*释放栈空间*/
     disposeStack(s);
 
     return 0;
